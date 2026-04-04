298a0695acefe9e2f2215ba988b1caea41140d22
chirag <chirag.peacockindia@gmail.com>
2026-04-04 09:41:50 +0530

    fix: update Firebase config to the correct bhagavad-gita-pwa project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHPE8eR5MqHqW2JNVEXWtcmG3bUYiy_WA",
-  authDomain: "bglite-6bcdd.firebaseapp.com",
-  projectId: "bglite-6bcdd",
-  storageBucket: "bglite-6bcdd.firebasestorage.app",
-  messagingSenderId: "669843995088",
-  appId: "1:669843995088:web:87fe3a687877998c9021d5",
-  measurementId: "G-X5YFNYNW7Q",
+  apiKey: "AIzaSyDLecXV97GpSFjbReKspPD6XdVHWhLW_J4",
+  authDomain: "bhagavad-gita-pwa.firebaseapp.com",
+  projectId: "bhagavad-gita-pwa",
+  storageBucket: "bhagavad-gita-pwa.firebasestorage.app",
+  messagingSenderId: "669306804260",
+  appId: "1:669306804260:web:1e90997c92b344b20520b1",
+  measurementId: "G-LBCGN8Y37Q"
 };
 
 export const app = initializeApp(firebaseConfig);
